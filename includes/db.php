87eda083231ddff2703c3de489e378a3b840454f
chrynx <ralphmadriaga@gmail.com>
2018-05-08 16:37:04 +0100

    trying out defining the db variables

--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,16 @@
 <?php
 
-$db_host = 'localhost';
-$db_name = 'cms';
-$db_username = 'admin';
-$db_password = 'very-hard-password';
+$db['db_host'] = 'localhost';
+$db['db_name'] = 'cms';
+$db['db_username'] = 'admin';
+$db['db_password'] = 'very-hard-password';
+
+foreach($db as $key => $value){
+    define(strtoupper($key), $value);
+}
 
 try {
-    $DB = new PDO("mysql:host=$db_host;dbname=$db_name",$db_username,$db_password);
+    $DB = new PDO("mysql:host=DB_HOST;dbname=DB_NAME",DB_USERNAME,DB_PASSWORD);
 } catch(PDOException $e) {
     echo "Connection Failed: " . $e->getMessage();
 }
