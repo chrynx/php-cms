807694b49b37bd3b537673034af0ffe7a85de307
chrynx <ralphmadriaga@gmail.com>
2018-05-08 16:23:58 +0100

    connected the DB

---
+++ includes/db.php
@@ -0,0 +1,13 @@
+<?php
+
+$db_host = 'localhost';
+$db_name = 'cms';
+$db_username = 'admin';
+$db_password = 'very-hard-password';
+
+try {
+    echo 'we are connected';
+    $DB = new PDO("mysql:host=$db_host;dbname=$db_name",$db_username,$db_password);
+} catch(PDOException $e) {
+    echo "Connection Failed: " . $e->getMessage();
+}
