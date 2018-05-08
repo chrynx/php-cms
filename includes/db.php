c5527a9d0b42017989cf34ca602e45815c6e167f
chrynx <ralphmadriaga@gmail.com>
2018-05-08 16:59:09 +0100

    imported the bootstrap theme

--- includes/db.php
+++ includes/db.php
@@ -1,16 +1,17 @@
 <?php
-
 $db['db_host'] = 'localhost';
 $db['db_name'] = 'cms';
 $db['db_user'] = 'admin';
 $db['db_pass'] = 'very-hard-password';
 
-foreach($db as $key => $value){
+foreach($db as $key => $value) {
     define(strtoupper($key), $value);
 }
-echo DB_HOST;
+
 try {
     $DB = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);
 } catch(PDOException $e) {
-    echo "Connection Failed: " . $e->getMessage();
+    echo 'Connection Failed: ' . $e->getMessage();
 }
+
+?>
