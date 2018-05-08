0ad5d95b63adcf2fb3c891916156de33a5d19141
chrynx <ralphmadriaga@gmail.com>
2018-05-08 16:42:23 +0100

    third attempt at constants

--- includes/db.php
+++ includes/db.php
@@ -2,15 +2,15 @@
 
 $db['db_host'] = 'localhost';
 $db['db_name'] = 'cms';
-$db['db_username'] = 'admin';
-$db['db_password'] = 'very-hard-password';
+$db['db_user'] = 'admin';
+$db['db_pass'] = 'very-hard-password';
 
 foreach($db as $key => $value){
     define(strtoupper($key), $value);
 }
 echo DB_HOST;
 try {
-    $DB = new PDO("mysql:host=DB_HOST;dbname=DB_NAME",DB_USERNAME,DB_PASSWORD);
+    $DB = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);
 } catch(PDOException $e) {
     echo "Connection Failed: " . $e->getMessage();
 }
